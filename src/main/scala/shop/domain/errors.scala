858b08b6cc61b54ff847838b1eb509134c474731
AnneThor <anne.thor@gmail.com>
2022-09-08 00:38:10 -0700

    draft central biz logic

--- src/main/scala/shop/domain/errors.scala
+++ src/main/scala/shop/domain/errors.scala
@@ -2,4 +2,7 @@ package shop.domain
 
 object errors {
 
+  trait ShoppingCartError extends Throwable
+  case object EmptyCartError extends ShoppingCartError
+
 }
--- src/main/scala/shop/programs/Checkout.scala
+++ src/main/scala/shop/programs/Checkout.scala
@@ -1,3 +1,35 @@
 package shop.programs
 
-case class Checkout()
+import cats.MonadThrow
+import cats.data.NonEmptyList
+import shop.domain.checkout.Card
+import shop.domain.errors.EmptyCartError
+import shop.domain.order.OrderId
+import shop.domain.payment.Payment
+import shop.domain.user.UserId
+import shop.services.{Orders, PaymentClient, ShoppingCart}
+import cats.syntax.all._
+
+final case class Checkout[F[_]: MonadThrow](
+  payments: PaymentClient[F],
+  cart: ShoppingCart[F],
+  orders: Orders[F]
+  ) {
+
+  private def ensureNonEmpty[A](xs: List[A]): F[NonEmptyList[A]] =
+    MonadThrow[F].fromOption(
+      NonEmptyList.fromList(xs),
+      EmptyCartError
+    )
+
+  def process(userId: UserId, card: Card): F[OrderId] = {
+    for {
+      cartTotal <- cart.get(userId)
+      cartItems <- ensureNonEmpty(cartTotal.items)
+      pid <- payments.process(Payment(userId, cartTotal.total, card))
+      oid <- orders.create(userId, pid, cartItems, cartTotal.total)
+      _ <- cart.delete(userId).attempt.void
+    } yield oid
+  }
+
+}
